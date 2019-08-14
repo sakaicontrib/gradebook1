977b51c34d0112a5a11481f8bcfb851e24d2b717
Earle Nietzel <earle@longsight.com>
2019-08-14 11:44:23 -0400

    Remove new API changes from Sakai 20 caused build to fail for 19 (#13)
    fixes #12
    
    (cherry picked from commit a11aac3fe1513e69891fa23c5d1cbc878728bdd3)

--- app/business/src/java/org/sakaiproject/tool/gradebook/framework/GradebookContextObserver.java
+++ app/business/src/java/org/sakaiproject/tool/gradebook/framework/GradebookContextObserver.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.sakaiproject.gradebook.framework;
+package org.sakaiproject.tool.gradebook.framework;
 
 import lombok.extern.slf4j.Slf4j;
 import org.sakaiproject.entity.api.ContextObserver;
